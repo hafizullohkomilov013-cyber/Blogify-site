98900a7f63b7546a020a3feea4f6bc3e49bdf70c
hafizullohkomilov013-cyber <hafizullohkomilov013@gmail.com>
2026-01-14 10:01:46 +0500

    the and

--- src/Pages/public/HomePage.jsx
+++ src/Pages/public/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../../components/Hero'
+import WhyChose from '../../components/WhyChose'
 
 function HomePage() {
   return (
     <>
         <Hero/>
+        <WhyChose/>
     </>
   )
 }
--- src/components/WhyChose.jsx
+++ src/components/WhyChose.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 function WhyChose() {
   return (
-    <section className='my-32'>
-      <div>
-        <h2>Why Choose Blogify?</h2>
-        <p>
+    <section className="my-32">
+      <div className="text-center">
+        <h2 className="text-[#0F1729] text-[36px] mb-3 font-bold tracking-tight">
+          Why Choose Blogify?
+        </h2>
+        <p className="w-full max-w-2xl m-auto text-[18px] text-gray-500">
           Built with modern technologies and best practices to provide the best
           blogging experience.
         </p>
