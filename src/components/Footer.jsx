93a0832620f4584aa6753d76e670c28481b173db
hafizullohkomilov013-cyber <hafizullohkomilov013@gmail.com>
2026-01-15 10:54:49 +0500

    the and

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <footer>
-      <section className="container gap-8 flex justify-between">
+    <footer className="border-t-2 border-gray-300">
+      <section className="container gap-8 pt-12 pb-8 border-b-2 mb-10 border-gray-200 flex justify-between">
         <div className="flex flex-col h-full min-h-34  gap-4 w-full max-w-148 items-start">
           <img src={NavLogo} alt="Footer-Logo" />
           <p className="text-[#6B7280] w-full max-w-[384px] text-[16px]">
@@ -18,20 +18,29 @@ function Footer() {
             Quick Links
           </h2>
           <div className="flex flex-col gap-2 text-[#6B7280]">
-            <Link className='link-cursor' to={"/"}>Home</Link>
-            <Link className='link-cursor' to={"/PostPages"}>Posts</Link>
-            <Link className='link-cursor' to={"/"}>Login</Link>
+            <Link className="link-cursor" to={"/"}>
+              Home
+            </Link>
+            <Link className="link-cursor" to={"/PostPages"}>
+              Posts
+            </Link>
+            <Link className="link-cursor" to={"/"}>
+              Login
+            </Link>
           </div>
         </div>
         <div className="w-full max-w-70 ">
           <h2 className="text-[#0F1729] text-[24px] mb-4 font-bold">Connect</h2>
           <div className="flex gap-3">
-            <i  className="text-2xl cursor-pointer link-cursor  fa-brands fa-twitter"></i>
-            <i  className="text-2xl cursor-pointer link-cursor  fa-brands fa-github"></i>
-            <i  className="text-2xl cursor-pointer link-cursor  fa-brands fa-linkedin"></i>
+            <i className="text-2xl cursor-pointer link-cursor  fa-brands fa-twitter"></i>
+            <i className="text-2xl cursor-pointer link-cursor  fa-brands fa-github"></i>
+            <i className="text-2xl cursor-pointer link-cursor  fa-brands fa-linkedin"></i>
           </div>
         </div>
       </section>
+      <div className="text-center pt-8 mb-12">
+        <p className="text-[#6B7280] text-[16px]">© 2026 Blogify. All rights reserved.</p>
+      </div>
     </footer>
   );
 }
