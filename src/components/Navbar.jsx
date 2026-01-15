a6ea87dc85841cc4b408e3b969add4d9db71874d
hafizullohkomilov013-cyber <hafizullohkomilov013@gmail.com>
2026-01-15 13:28:34 +0500

    the and

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,12 +9,12 @@ function Navbar() {
     const [open, setOpen] = useState(false) 
 
     return (
-    <nav className='pt-4 pb-4'>
-        <div className='container flex justify-between items-center'>
+    <nav className='pt-4 pb-4 fixed right-0 left-0 top-0 backdrop-blur-md bg-white/70'>
+        <div className='container   flex justify-between items-center'>
             <img src={NavLogo} alt="Bilogyfay-logo" />
             <div className='hidden md:flex gap-8 justify-center items-center'>
-                <Link className='font-medium link-cursor text-4 text-[#0F1729] ' to="/">Home</Link>
-                <Link className='font-medium link-cursor text-4 text-[#0F1729] ' to="/PostPages">Posts</Link>
+                <Link className='font-medium link-cursor text-[16px] text-[#0F1729] ' to="/">Home</Link>
+                <Link className='font-medium link-cursor text-[16px] text-[#0F1729] ' to="/PostPages">Posts</Link>
                 <Link className='font-medium text-4 text-white rounded-xl w-16 h-9 rounded-1xl bg-[#4346EF] flex justify-center items-center' to="/">Login</Link>
             </div>
             <div onClick={() => {setOpen(!open)}} className='md:hidden'>
